0447c077b4d02f63e7990d4b09526fe0d4ba800c
slim <slim@whoi.edu>
2018-10-30 09:27:08 -0400

    changed to generic analyzehsmis.py in order to also have cell dimension analysis

---
+++ analyzehsmis.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import numpy as np
+from glob import glob
+from scipy import signal, stats
+import matplotlib.pyplot as plt
+
+
+def analyzepath(file):
+    """ Input: file with the X and Y coordinates from a cell's path (_path.csv)
+        Output: path duration in s, path length in pixels, path-averaged speed in pixels/s,and net to gross displacement ratio
+    """
+    path = pd.read_csv(file)
+
+    nframes = np.shape(path)[0]
+    tswim = (nframes-1)/2000.0  # path duration  # camera fps = 2000
+
+    # smooth X and Y coordinates with Savitzky-Golay filter; got window length and polynomial order from Houshuo
+    # keeps same number of data points so nframes is still relevant
+    smoothx = signal.savgol_filter(path['X'], 27, 4)
+    smoothy = signal.savgol_filter(path['Y'], 27, 4)
+
+    # creates a list with point to point distances
+    pathlist = []
+    for n in range(0,nframes-1):
+        x1 = smoothx[n]
+        y1 = smoothy[n]
+        x2 = smoothx[n+1]
+        y2 = smoothy[n+1]
+        xsqd = (x2-x1)**2
+        ysqd = (y2-y1)**2
+        pathlist.append(np.sqrt(xsqd + ysqd))
+
+    lswim = sum(pathlist)  # aka gross displacement or path length
+
+    Uave = lswim/tswim  # path-averaged speed
+
+    xi = path.iloc[0]['X']
+    yi = path.iloc[0]['Y']
+    xf = path.iloc[-1]['X']
+    yf = path.iloc[-1]['Y']
+    xsqd = (xf-xi)**2
+    ysqd = (yf-yi)**2
+    ND = np.sqrt(xsqd + ysqd)  # net displacement
+    NGDR = ND/lswim
+
+    return tswim, lswim, Uave, NGDR
+
+
+def analyzepath_folder(directory):
+    """ Input: folder with several files of the X and Y coordinates from a cell's path (_path.csv)
+        Output: pandas dataframe with path duration in s, path length in mm, path-averaged speed in mm/s and of net to gross displacement ratio
+    """
+    folder = sorted(glob(directory))  # glob does not order, so if we want entries to match up between analyzepath_folder and analyzecell_folder, needs to be sorted
+    tswim = []
+    lswim = []
+    Uave = []
+    NGDR = []
+    for file in folder:
+        results = analyzepath(file)
+        tswim.append(results[0])
+        lswim.append(results[1] *0.7/912.020)  # conversion from pixels to mm for 20x lens
+        Uave.append(results[2] *0.7/912.020)
+        NGDR.append(results[3])
+    summary = np.transpose(pd.DataFrame([tswim, lswim, Uave, NGDR]))
+    summary.columns = ['tswim', 'lswim', 'uave', 'ngdr']
+    return summary
+
+
+def analyzecell_folder(directory):
+    """ Input: folder with several files of cells' dimensions (_focalcell.csv)
+        Output: pandas dataframe with cell length and width in mm
+    """
+    folder = sorted(glob(directory))  # glob does not order, so if we want entries to match up between analyzepath_folder and analyzecell_folder, needs to be sorted
+    length = []
+    width = []
+    for file in folder:
+        celldata = pd.read_csv(file)
+        if 'L' in celldata:  # this first part is only necessary because of the csv formatting that I had done manually at the beginning of the FC analysis
+            length.append(celldata['L'][0] *0.7/912.020)  # conversion from pixels to mm for 20x lens
+            width.append(celldata['W'][0] *0.7/912.020)
+        else:  # once I started using the macro, this is sufficient
+            length.append(celldata['Roi_Length'][0] *0.7/912.020)
+            width.append(celldata['Roi_Width'][0] *0.7/912.020)
+    summary = np.transpose(pd.DataFrame([length, width]))
+    summary.columns = ['length', 'width']
+    return summary
--- fc_analysis.py
+++ fc_analysis.py
@@ -4,28 +4,37 @@
 # define functions
 exec(open('./analyzepath.py').read())
 
-# calculate speed and net to gross displacement for four days of data
-Uave24, NGDR24 = analyzepath_folder('/Volumes/GoogleDrive/My Drive/data/hsmis/fiddlerscove/Fiddlers_20180724/*/*_path.csv')
-Uave26, NGDR26 = analyzepath_folder('/Volumes/GoogleDrive/My Drive/data/hsmis/fiddlerscove/Fiddlers_20180726/*/*_path.csv')
-Uave30, NGDR30 = analyzepath_folder('/Volumes/GoogleDrive/My Drive/data/hsmis/fiddlerscove/Fiddlers_20180730/*/*_path.csv')
-Uave31, NGDR31 = analyzepath_folder('/Volumes/GoogleDrive/My Drive/data/hsmis/fiddlerscove/Fiddlers_20180731/*/*_path.csv')
+# calculate path duration, path length, path-averaged speed, and net to gross displacement ratio for four days of data
+path24 = analyzepath_folder('/Volumes/GoogleDrive/My Drive/data/hsmis/fiddlerscove/Fiddlers_20180724/*/*_path.csv')
+path26 = analyzepath_folder('/Volumes/GoogleDrive/My Drive/data/hsmis/fiddlerscove/Fiddlers_20180726/*/*_path.csv')
+path30 = analyzepath_folder('/Volumes/GoogleDrive/My Drive/data/hsmis/fiddlerscove/Fiddlers_20180730/*/*_path.csv')
+path31 = analyzepath_folder('/Volumes/GoogleDrive/My Drive/data/hsmis/fiddlerscove/Fiddlers_20180731/*/*_path.csv')
 
-# take results from above and combine into lists so that the mean and SEM can be found for each day (because a day has many tracks)
-Uaves = [Uave24, Uave26, Uave30, Uave31]
-NGDRs = [NGDR24, NGDR26, NGDR30, NGDR31]
-Uave_means = [np.mean(list) for list in Uaves]
-NGDR_means = [np.mean(list) for list in NGDRs]
-Uave_sem = [stats.sem(list) for list in Uaves]
-NGDR_sem = [stats.sem(list) for list in NGDRs]
+# take results from above and combine into a single list so that the mean, std, and SEM can be quickly found for each day (because a day has many tracks)
+paths = [path24, path26, path30, path31]
+tswim_means = [np.mean(df['tswim']) for df in paths]
+lswim_means = [np.mean(df['lswim']) for df in paths]
+uave_means = [np.mean(df['uave']) for df in paths]
+ngdr_means = [np.mean(df['ngdr']) for df in paths]
+
+tswim_stdev = [np.std(df['tswim']) for df in paths]
+lswim_stdev = [np.std(df['lswim']) for df in paths]
+uave_stdev = [np.std(df['uave']) for df in paths]
+ngdr_stdev = [np.std(df['ngdr']) for df in paths]
+
+lswim_sem = [stats.sem(df['lswim']) for df in paths]
+tswim_sem = [stats.sem(df['tswim']) for df in paths]
+uave_sem = [stats.sem(df['uave']) for df in paths]
+ngdr_sem = [stats.sem(df['ngdr']) for df in paths]
 
 # plotting
 labels = ['Jul 24', 'Jul 26', 'Jul 30', 'Jul 31']
-plt.bar(range(0,4), Uave_means, tick_label=labels, yerr=Uave_sem)
+plt.bar(range(0,4), uave_means, tick_label=labels, yerr=uave_sem)
 plt.ylabel('Uave (mm/s)')
 
-plt.bar(range(0,4), NGDR_means, tick_label=labels, yerr=NGDR_sem)
+plt.bar(range(0,4), ngdr_means, tick_label=labels, yerr=ngdr_sem)
 plt.ylabel('NGDR')
 
 # due to sample size, I think you can only do statistics with the first 2 days
-stats.ttest_ind(Uave24, Uave26, equal_var=True)
-stats.ttest_ind(NGDR24, NGDR26, equal_var=True)
+stats.ttest_ind(uave24, uave26, equal_var=True)
+stats.ttest_ind(ngdr24, ngdr26, equal_var=True)
